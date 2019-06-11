ece2de8bb31265ba152082c9cfa3cb3e9854ce76
Mantenal <carlosloco41@gmail.com>
2019-06-11 10:24:54 -0500

    Medio Programa funcionando
    Mitad de las caracteristicas de empleado funcionando

--- Sistema de Analisis Clinico/Sistema de Analisis Clinico/Sistema de Analisis Clinico.cpp
+++ Sistema de Analisis Clinico/Sistema de Analisis Clinico/Sistema de Analisis Clinico.cpp
@@ -89,21 +89,69 @@ public:
 
 class C_Personal : public R_Materiales {
 private:
-	string nombre;
-	int edad;
-	int ine;
-	int codigo;
-	int tipo;
-	float sueldo;
+	string Nombre;
+	int Edad;
+	int Ine;
+	int Codigo;
+	int Tipo;
+	float Sueldo;
 public:
 	void agregar();
-	void reporte();
-	void buscar();
+	void buscar(bool todo);
 	void Modificar();
 	void eliminar();
 	//funcion de agregar heredada de R_materiales
 
+}personal;
+
+
+
+class Nodo_Personal {
+	string Nombre;
+	int Edad;
+	int Ine;
+	int Codigo;
+	string Tipo;
+	float Sueldo;
+	Nodo_Personal *ant, *sig;
+public:
+	Nodo_Personal(string nombre, int edad, int ine, int codigo, string tipo, float sueldo) {
+		Nombre = nombre;
+		Edad = edad;
+		Ine = ine;
+		Codigo = codigo;
+		Tipo = tipo;
+		Sueldo = sueldo;
+	};
+	friend class ListaPersonal;
 };
+
+
+
+class ListaPersonal {
+	Nodo_Personal *raiz;
+public:
+	ListaPersonal() { raiz = NULL; };
+	~ListaPersonal() {
+		Nodo_Personal *reco = raiz;
+		Nodo_Personal *bor;
+		while (reco != NULL)
+		{
+			bor = reco;
+			reco = reco->sig;
+			delete bor;
+		}
+	};
+	void insertarPrimero(string nombre, int edad, int ine, int codigo, string tipo, float sueldo);
+	void imprimir();
+};
+
+
+
+
+
+
+/***************************************************************************************************************************************/
 class M_Clientes {
 protected:
 	string nombre;
@@ -138,7 +186,7 @@ public:
 int main()
 {
 	char opc, opc2;
-	
+
 	do
 	{
 		system("cls");
@@ -208,12 +256,15 @@ int main()
 			{
 			case'1':
 				// agregar empleado
+				personal.agregar();
 				break;
 			case'2':
 				//reporte de empleado
+				personal.buscar(true);
 				break;
 			case'3':
 				//Busca un empleado
+				personal.buscar(false);
 				break;
 			case'4':
 				//modificar datos 
@@ -286,6 +337,9 @@ int main()
 }
 
 
+/***************************************************************************************************************************************************************************/
+/*fUNCIONES PARA MATERIALES*/
+
 void R_Materiales::alta() {
 
 	system("cls");
@@ -331,8 +385,8 @@ void R_Materiales::buscar(bool buscar, int dato)
 	ListaMateriales *lista1 = new ListaMateriales();
 
 	int nreg;
-	string nombre,nombre2;
-
+	string nombre, nombre2;
+	bool encontrado = false;
 	ifstream salida;
 	salida.open("Materiales.dat", ios::in | ios::binary);
 
@@ -359,7 +413,7 @@ void R_Materiales::buscar(bool buscar, int dato)
 				}
 
 			}
-			
+
 		}
 
 
@@ -367,15 +421,15 @@ void R_Materiales::buscar(bool buscar, int dato)
 
 		else
 		{
-			if (dato==1)
+			if (dato == 1)
 			{
 				cout << "Ingresa el Nombre del Producto a Buscar" << endl;
 				getline(cin, nombre);
 				getline(cin, nombre);
 
-		
+
 				std::transform(nombre.begin(), nombre.end(), nombre.begin(), ::toupper);
-			
+
 
 				for (int x = 0;x < nreg;x++)
 				{
@@ -386,8 +440,9 @@ void R_Materiales::buscar(bool buscar, int dato)
 
 					if (nombre == nombre2) {
 						lista1->insertarPrimero(materiales.Codigo, materiales.Tipo, materiales.Nombre, materiales.Existencia, materiales.Precio);
-
+						encontrado = true;
 					}
+
 
 				}
 
@@ -395,11 +450,12 @@ void R_Materiales::buscar(bool buscar, int dato)
 			}
 		}
 
-
-
-
 		salida.close();
 		lista1->imprimir();
+		if (encontrado == false && buscar == true)
+		{
+			cout << "No se encontro Producto" << endl;
+		}
 		system("pause");
 
 	}
@@ -410,6 +466,8 @@ void R_Materiales::buscar(bool buscar, int dato)
 
 void ListaMateriales::imprimir()
 {
+
+	system("cls");
 	Nodo_Materiales *reco = raiz;
 	while (reco != NULL)
 	{
@@ -498,4 +556,191 @@ void R_Materiales::modificar()
 		system("pause");
 
 	}
+}
+/***************************************************************************************************************************************************************************/
+
+
+
+
+
+void C_Personal::agregar() {
+
+	system("cls");
+	ofstream entrada;
+	entrada.open("Personal.dat", ios::out | ios::app | ios::binary);
+
+	if (entrada.fail())
+	{
+		cout << "error al crear archivo";
+		system("pause");
+	}
+
+	else
+	{
+
+
+		string Nombre;
+		int Edad;
+		int Ine;
+		int Codigo;
+		int Tipo;
+		float Sueldo;
+
+		cout << "Ingresa el Nombre:" << endl;//revisar aqui el tipo de dato string puede dar problemas
+		getline(cin, personal.Nombre);
+		getline(cin, personal.Nombre);
+		cout << "Ingresa la Edad:" << endl;
+		cin >> personal.Edad;
+		cout << "Ingresa el Numero de la INE:" << endl;
+		cin >> personal.Ine;
+		cout << "Ingresar Codigo:" << endl;
+		cin >> personal.Codigo;
+
+		do
+		{
+			cout << "ingresa el tipo:" << endl;
+			cout << "1-Administrativos" << endl;
+			cout << "2-Laboratoristas" << endl;
+			cin >> personal.Tipo;
+		} while (personal.Tipo != 1 && personal.Tipo != 2);
+
+		cout << "Ingresar Sueldo:" << endl;
+		cin >> personal.Sueldo;
+
+		entrada.write((char *)&personal, sizeof(C_Personal));
+		entrada.close();
+
+	}
+
+}
+
+
+
+
+
+void C_Personal::buscar(bool todo)
+{
+
+	ListaPersonal *lista2 = new ListaPersonal();
+
+	int nreg,codigo;
+	bool encontrar = false;
+
+	ifstream salida;
+	salida.open("Personal.dat", ios::in | ios::binary);
+
+	if (salida.fail())
+	{
+		cout << "error al abrir el archivo" << endl;
+		system("pause");
+	}
+
+
+
+
+
+
+	else
+	{
+		salida.seekg(0, ios::end);
+		nreg = salida.tellg() / sizeof(C_Personal);
+		salida.seekg(0);
+
+		if (todo)
+		{
+
+			for (int x = 0;x < nreg;x++)
+			{
+				salida.read((char *)&personal, sizeof(C_Personal));
+
+				if (personal.Tipo == 1) {
+					lista2->insertarPrimero(personal.Nombre, personal.Edad, personal.Ine, personal.Codigo, "Admistrativo", personal.Sueldo);
+				}
+
+				else {
+					lista2->insertarPrimero(personal.Nombre, personal.Edad, personal.Ine, personal.Codigo, "Laboratorista", personal.Sueldo);
+				}
+
+			}
+
+		}
+
+		else
+		{
+
+			cout << "Ingresa El codigo del Empleado a Buscar" << endl;
+			cin >> codigo;
+
+			for (int x = 0;x < nreg;x++)
+			{
+				salida.read((char *)&personal, sizeof(C_Personal));
+
+				if (personal.Codigo == codigo)
+				{
+					encontrar = true;
+					if (personal.Tipo == 1) {
+						lista2->insertarPrimero(personal.Nombre, personal.Edad, personal.Ine, personal.Codigo, "Admistrativo", personal.Sueldo);
+					}
+
+					else {
+						lista2->insertarPrimero(personal.Nombre, personal.Edad, personal.Ine, personal.Codigo, "Laboratorista", personal.Sueldo);
+					}
+				}
+			}
+
+
+		}
+
+	}
+
+	salida.close();
+	lista2->imprimir();
+
+	if (encontrar == false && todo == false)
+	{
+		cout << "No se encontro Producto" << endl;
+	}
+	system("pause");
+
+
+	delete lista2;
+}
+
+
+
+void ListaPersonal::insertarPrimero(string nombre, int edad, int ine, int codigo, string tipo, float sueldo)
+{
+	Nodo_Personal *nuevo = new Nodo_Personal(nombre, edad, ine, codigo, tipo, sueldo);
+	nuevo->ant = NULL;
+	if (raiz == NULL)
+	{
+		nuevo->sig = NULL;
+		raiz = nuevo;
+	}
+	else
+	{
+		nuevo->sig = raiz;
+		raiz->ant = raiz;
+		raiz = nuevo;
+	}
+}
+
+
+void ListaPersonal::imprimir()
+{
+	system("cls");
+	Nodo_Personal *reco = raiz;
+	while (reco != NULL)
+	{
+		cout << "Nombre: " << reco->Nombre << endl;;
+
+		cout << "Edad: " << reco->Edad << endl;
+		cout << "Ine: " << reco->Ine << endl;
+		cout << "Codigo: " << reco->Codigo << endl;
+		cout << "Tipo: " << reco->Tipo << endl;
+		cout << "Sueldo: " << reco->Sueldo << endl << endl;
+
+		reco = reco->sig;
+	}
+	cout << "\n";
 }
